fc7459ffbb67635d2d85c5f4af5b52ae10a3ec8b
Admin <yevgen.tsyganenko@gmail.com>
2015-06-12 12:38:08 +0300

    TSYSquare updated. randomPosition and nextPosition fixed

--- TSYHomeworkUI/Source/TSYViewController/TSYView.m
+++ TSYHomeworkUI/Source/TSYViewController/TSYView.m
@@ -22,36 +22,57 @@
 }
 
 - (void)randomPosition {
-    self.square.backgroundColor = [UIColor greenColor];
+    self.position = [self randomFuturePosition];
 }
 
 - (void)setPosition:(TSYSquarePosition)position {
     if (_position != position) {
-        CGRect newFrame = self.square.frame;
-        
-        CGFloat newFrameX = newFrame.origin.x;
-        CGFloat newFrameY = newFrame.origin.y;
-        
-        CGFloat distanceX = self.window.bounds.size.width - self.square.frame.size.width;
-        CGFloat distanceY = self.window.bounds.size.height - self.square.frame.size.height;
-        
-        if (TSYSquarePositionRightUp == position) {
-            newFrame.origin = CGPointMake(distanceX, newFrameY);
-        } else if (TSYSquarePositionRightDown == position) {
-            newFrame.origin = CGPointMake(newFrameX, distanceY);
-        } else if (TSYSquarePositionLeftDown == position) {
-            newFrame.origin = CGPointMake(newFrameX-distanceX, newFrameY);
-        } else if (TSYSquarePositionLeftUp == position) {
-            newFrame.origin = CGPointMake(newFrameX, newFrameY-distanceY);
-        }
-        
+        CGRect frame = [self frameWithPosition:position];
+
         [UIView animateWithDuration:1
                          animations:^{
-                             self.square.frame = newFrame;
+                             self.square.frame = frame;
                          }];
-        
+
         _position = position;
     }
+}
+
+- (CGRect)frameWithPosition:(TSYSquarePosition)position {
+    CGRect windowFrame = self.window.bounds;
+    CGRect squareFrame = self.square.frame;
+    CGRect newFrame = CGRectZero;
+    newFrame.size = squareFrame.size;
+    
+    CGFloat distanceX = windowFrame.size.width - squareFrame.size.width;
+    CGFloat distanceY = windowFrame.size.height - squareFrame.size.height;
+    
+    switch (position) {
+        case TSYSquarePositionLeftUp:
+            newFrame.origin = CGPointMake(windowFrame.origin.x, windowFrame.origin.y);
+            
+            return newFrame;
+            
+        case TSYSquarePositionRightUp:
+            newFrame.origin = CGPointMake(windowFrame.origin.x + distanceX, windowFrame.origin.y);
+
+            return newFrame;
+            
+        case TSYSquarePositionRightDown:
+            newFrame.origin = CGPointMake(windowFrame.origin.x + distanceX, windowFrame.origin.y + distanceY);
+            
+            return newFrame;
+            
+        case TSYSquarePositionLeftDown:
+            newFrame.origin = CGPointMake(windowFrame.origin.x, windowFrame.origin.y + distanceY);
+            
+            return newFrame;
+            
+        default:
+            return squareFrame;
+    }
+    
+    return CGRectNull;
 }
 
 - (TSYSquarePosition)futurePosition {
@@ -60,8 +81,18 @@
     if (TSYSquarePositionLeftDown == currentPosition) {
         return TSYSquarePositionLeftUp;
     }
-    
+
     return currentPosition + 1;
+}
+
+- (TSYSquarePosition)randomFuturePosition {
+    TSYSquarePosition randomPosition = arc4random_uniform(TSYSquarePositionLeftDown + 1);
+    
+    if (randomPosition == self.position) {
+        randomPosition = [self randomFuturePosition];
+    }
+
+    return randomPosition;
 }
 
 @end
