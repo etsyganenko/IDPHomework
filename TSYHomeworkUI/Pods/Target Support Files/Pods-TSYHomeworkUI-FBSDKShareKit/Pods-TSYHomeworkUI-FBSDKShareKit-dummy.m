9f37efe61245c4f024beea4862b063a1f367cbfa
Admin <yevgen.tsyganenko@gmail.com>
2015-07-23 23:51:18 +0300

    FBSDKCoreKit, FBSDKLoginKit, FBSDKShareKit installed

---
+++ TSYHomeworkUI/Pods/Target Support Files/Pods-TSYHomeworkUI-FBSDKShareKit/Pods-TSYHomeworkUI-FBSDKShareKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TSYHomeworkUI_FBSDKShareKit : NSObject
+@end
+@implementation PodsDummy_Pods_TSYHomeworkUI_FBSDKShareKit
+@end
---
+++ TSYHomeworkUI/Pods/Target Support Files/Pods-TSYHomeworkUI/Pods-TSYHomeworkUI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TSYHomeworkUI : NSObject
+@end
+@implementation PodsDummy_Pods_TSYHomeworkUI
+@end
