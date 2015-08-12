db82c954a3a7879df7959f0ac8a153ba3e35029f
Yevgen <yevgen.tsyganenko@idapgroup.com>
2015-08-12 16:59:34 +0300

    TSYPhotosViewCell - initial commit

---
+++ TSYHomeworkUI/Source/UI/TSYPhotosViewController/TSYPhotosViewCell/TSYPhotosViewCell.h
@@ -0,0 +1,16 @@
+//
+//  TSYPhotosViewCell.h
+//  TSYHomeworkUI
+//
+//  Created by Yevgen on 8/12/15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TSYImageView.h"
+
+@interface TSYPhotosViewCell : UITableViewCell
+@property (nonatomic, strong)   IBOutlet TSYImageView       *photoImageView;
+
+@end
---
+++ TSYHomeworkUI/Source/UI/TSYPhotosViewController/TSYPhotosViewCell/TSYPhotosViewCell.m
@@ -0,0 +1,21 @@
+//
+//  TSYPhotosViewCell.m
+//  TSYHomeworkUI
+//
+//  Created by Yevgen on 8/12/15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import "TSYPhotosViewCell.h"
+
+@implementation TSYPhotosViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+}
+
+@end
